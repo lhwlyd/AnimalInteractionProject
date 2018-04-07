39df4c37eee8175cea30245a10180a2b13f85467
Roy Y. Luo <yil623@ucsd.edu>
2018-04-07 15:38:54 -0700

    Added basic behaviors scripts

---
+++ Assets/_Scripts/_Animals/BaseAnimal.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseAnimal: MonoBehaviour {
+    float agressionLevel;
+    float intimateLevel;
+    float intelligenceLevel;
+    float hungerLevel;
+    float thirstLevel;
+    float healthLevel;
+    
+    public abstract void Move();
+
+    public abstract void Eat();
+
+    public abstract void Drink();
+
+    public abstract void Injured(float damage);
+
+} 
