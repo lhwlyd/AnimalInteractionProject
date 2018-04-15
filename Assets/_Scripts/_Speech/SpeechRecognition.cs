64a3d99d92beec8034dfdc23e1943b41fa23fee6
Carlos Chulo <thecompscientist.dev@gmail.com>
2018-04-15 16:49:49 -0700

    researching voice recognition techniques, so far everything windows supports is only available in windows 10

---
+++ Assets/_Scripts/_Speech/SpeechRecognition.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine.Windows.Speech;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class SpeechRecognition : MonoBehaviour {
+
+    public KeywordRecognizer keywordRecognizer;
+    public Dictionary<string, System.Action> keywords = new Dictionary<string, System.Action>();
+
+    // Use this for initialization
+    void Start () {
+        //Create keywords for keyword recognizer
+        keywords.Add("activate", () =>
+        {
+            Debug.Log("ITS ACTIVATING");
+            // action to be performed when this keyword is spoken
+        });
+
+        keywordRecognizer = new KeywordRecognizer(keywords.Keys.ToArray());
+        keywordRecognizer.OnPhraseRecognized += KeywordRecognizer_OnPhraseRecognized;
+        keywordRecognizer.Start();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void KeywordRecognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        System.Action keywordAction;
+        // if the keyword recognized is in our dictionary, call that Action.
+        if (keywords.TryGetValue(args.text, out keywordAction))
+        {
+            keywordAction.Invoke();
+        }
+    }
+}
