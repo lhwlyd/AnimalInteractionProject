7d047acc075647c915a4955bf0c604391ad14500
Carlos Chulo <thecompscientist.dev@gmail.com>
2018-05-28 03:37:59 -0700

    attempted to implement input for vive

---
+++ Assets/_Scripts/_Interaction/Vive_LHandGesture.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class Vive_LHandGesture : MonoBehaviour, IHandGesture {
+
+    Vector3 previousPos = new Vector3();
+
+    private void Start()
+    {
+        previousPos = transform.position;
+    }
+
+    private void Update()
+    {
+        previousPos = transform.position;
+        Debug.Log("Left is grabbing: " + IsGrabbing());
+        Debug.Log("Left palm open: " + IsPalmOpen());
+    }
+
+    public Vector3 GetVelocity()
+    {
+        var currPos = transform.position;
+        var velocity = (currPos - previousPos) / Time.deltaTime;
+        previousPos = currPos;
+        return velocity;
+    }
+
+    public bool IsGrabbing()
+    {
+        return SteamVR_Controller.Input(0).GetHairTriggerDown();
+    }
+
+    public bool IsPalmOpen()
+    {
+        return SteamVR_Controller.Input(0).GetHairTriggerUp();
+    }
+}
---
+++ Assets/_Scripts/_Interaction/Vive_RHandGesture.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class Vive_RHandGesture : MonoBehaviour, IHandGesture
+{
+
+    Vector3 previousPos = new Vector3();
+
+    private void Start()
+    {
+        previousPos = transform.position;
+    }
+
+    private void Update()
+    {
+        previousPos = transform.position;
+        Debug.Log("Right is grabbing: " + IsGrabbing());
+        Debug.Log("Right palm open: " + IsPalmOpen());
+    }
+
+    public Vector3 GetVelocity()
+    {
+        var currPos = transform.position;
+        var velocity = (currPos - previousPos) / Time.deltaTime;
+        previousPos = currPos;
+        return velocity;
+    }
+
+    public bool IsGrabbing()
+    {
+        return SteamVR_Controller.Input(1).GetHairTriggerDown();
+    }
+
+    public bool IsPalmOpen()
+    {
+        return SteamVR_Controller.Input(1).GetHairTriggerUp();
+    }
+}
